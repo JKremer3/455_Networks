b034dfeb68bac0e0cd75bec5e187f1303275dbc9
JKremer3 <jeffrey.kremer.iii@gmail.com>
2019-10-26 14:02:29 -0700

    SendIP theoretically working, recieve does not

--- project3/455_proj3.py
+++ project3/455_proj3.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 from scapy.all import *
+from netaddr import *
 
 #broadcast = "ff:ff:ff:ff:ff:ff"
 
@@ -32,32 +33,99 @@ def createARPheader(destinationIP):
     return arpHeader
 
 def processARP(packet):
-    arpRes, noRes = srp(packet, timeout = 2)
+    arpRes = srp1(packet, timeout = 2)
 
     arpRes.show()
-    if(arpRes.res[0][1].sprintf("%Ether.dst%") == get_if_hwaddr(conf.iface)):
+    if(arpRes[0][Ether].dst == get_if_hwaddr(conf.iface)):
         print("ARP received")
-        arpRes.res[0][1].show()
+        arpRes[0].show()
+        return arpRes[0][Ether].dst
     else:
         print("Failed to Receive ARP")
+
+def sendIPpacket(interface, destinationIP, routerIP, message):
+    print("Interface: {}".format(interface))
+    print("destIP   : {}".format(destinationIP))
+    print("routerIP : {}".format(routerIP))
+    print("message  : {}".format(message))
+
+    raw_message = Raw(load = message)
+    
+    #first create and send ARP to get HW address
+    ethhdrArp = createEthernetHeader()
+    arphdr    = createARPheader(destinationIP)
+    arpPacket = ethhdrArp/arphdr #put together the arp packet
+
+    #send the arp to get the destination MAC
+    destMAC   = processARP(arpPacket)
+
+    #with the destination MAC, start building IP packet
+    ethhdrIP  = createEthernetHeader(destMAC)
+    iphdr     = IP(dst = destinationIP)
+    iphdr.ttl = 6
+
+    #construct the full packet
+    fullPack  = ethhdrIP/iphdr
+    fullPack.add_payload(raw_message)
+    fullPack.show()
+
+    #send the packet
+    sendp(fullPack)
+    
+def recvIPpacket(interface):
+    print("Interface: {}".format(interface))
+    while(1):
+        recievedPack = sniff()
+        recievedPack.show()
+        if(recievedPack[0].haslayer("Raw") and recievedPack[0][Ethernet].dst == get_if_hwaddr(conf.iface)):
+            #recievedPack.show()
+            print("message: {}".format(recievedPack[0][raw].load))
+            return
+
+def runDebug(param):
+    if(param == "ARP"):
+        print("Test sending an ARP packet")
+        ethhdr = createEthernetHeader()
+        arphdr = createARPheader("10.0.0.2")
+        packet = ethhdr/arphdr
+        ethhdr.show()
+        arphdr.show()
+        print("\nFull Packet:")
+        packet.show()
+        processARP(packet)
+    elif(param == "Send"):
+        print("Sending Packet on local network") 
+        sendIPpacket("h1-eth0","10.0.0.2", "10.0.0.1", "Hello World")
+    elif(param == "Recv"):
+        recvIPpacket("h2-eth0")
+    else:
+        print("Error: invalid debug command")
+
 
 if __name__ == "__main__":
     global debug
     debug = 0
     if(len(sys.argv) > 1 and sys.argv[1] == "debug"):
         debug = 1
-
-    getSystemInformation()
     
-    ethhdr = createEthernetHeader()
-    arphdr = createARPheader("10.0.0.2")
-    packet = ethhdr/arphdr
-    ethhdr.show()
-    arphdr.show()
-    print("\nFull Packet:")
-    packet.show()
+    if(len(sys.argv) == 6 and sys.argv[1] == "Send" and debug == 0):
+        interfaceHolder = sys.argv[2]
+        destinationIP   = sys.argv[3]
+        routerIP        = sys.argv[4]
+        message         = sys.argv[5]
+        sendIPpacket(interfaceHolder, destinationIP, routerIP, message)
+    elif(len(sys.argv) == 3 and sys.argv[1] == "Recv" and debug == 0):
+        interfaceHolder = sys.argv[2]
+        recvIPpacket(interfaceHolder)
+    elif(debug == 1):
+        if(len(sys.argv) == 2):
+            print("Error: No Parameter passed in\nPlease specify <ARP>, <Send>, or <Recv>")
+        else:
+            runDebug(sys.argv[2])
+    else:
+        print("Usage:  python3 ./455_proj3.py Send <InterfaceName> <DestIP> <Router IP> <Message>\n\tpython3 ./455_proj3.py Send <InterfaceName>") 
 
-    processARP(packet)
+    
 
 
 
