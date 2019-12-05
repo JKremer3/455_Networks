fbe6f16c3b01d1dc863f2edfb9cdd12c038c18e4
JKremer3 <jeffrey.kremer.iii@gmail.com>
2019-12-04 16:25:20 -0800

    Client and Server now handle Acks and Nacks, logic errors seems to be present

--- project4/client.py
+++ project4/client.py
@@ -2,6 +2,20 @@
 import socket
 from scapy.all import *
 
+def getLocalIP():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(("10.0.0.1", 80))
+
+    localIP = sock.getsockname()[0]
+    sock.close()
+
+    return localIP
+
+def sendWindow(window, sendIP, PORT, count):
+    while count < len(window):
+        data = window[count]
+        sock.sendto(bytes(str(count) + data, encoding='UTF-8'), (sendIP, PORT))
+        count+=1
 
 def generateWindows(data, maxWindowSize):
     windowHolder = []
@@ -13,14 +27,34 @@ def generateWindows(data, maxWindowSize):
             windowHolder.append(dataBuf)
             dataBuf = []
         #if the data buffer is smaller than the max size, but the current line is last line in the data set
-        elif len(dataBuf) < maxWindowSize and line == data[len(data)-1]:
-            dataBuf.append(line) 
+        elif len(dataBuf) < maxWindowSize and line == data[-1]:
+            dataBuf.append(line)
+            dataBuf.append("ffff") 
             windowHolder.append(dataBuf)
         #otherwise fill the buffer
         else:
             dataBuf.append(line)
-    
     return windowHolder
+
+def receiveAcknowledgment(recvIP, PORT, sock):
+    recv = None
+    sock.settimeout(6)
+
+    #while nothing has been recieved -
+    while (recv == None) or (recv == '\x00') or (recv == ''):
+        try:
+            recv, addr = sock.recvfrom(1024)
+            recv = recv.decode("UTF-8")
+
+            if recv == 'ACK':
+                return 10
+            elif recv[0] == 'N':
+                return int(recv[2])
+
+        except(socket.timeout):
+            print("Failed to Receive; Socket closing")
+            sock.close()
+            exit(1)
 
 if __name__ == "__main__":
     if(len(sys.argv) < 3):
@@ -28,26 +62,33 @@ if __name__ == "__main__":
         exit(1)
 
     windowSize = 10
-    UDP_IP = sys.argv[1]
-    UDP_PORT = 5005
+    IP = sys.argv[1]
+    PORT = 5005
     filename= sys.argv[2]
+    localIP = getLocalIP()
 
     file = open(filename, 'r+')
     lines = file.readlines()
 
-    print("UDP target IP:", UDP_IP)
-    print("UDP target port:", UDP_PORT)
-    #print("message:", MESSAGE)
+    print("IP:", localIP)
+    print("port:", PORT)
+    #print("message:", MESSAGE)print("Window Complete")print("Window Complete")
 
     sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
-    
+    sock.bind((localIP, PORT))
     windowKeeper = generateWindows(lines, windowSize)
 
+    #for each window
     for window in windowKeeper:
-        for line in window:
-            print(line, end='')
-            sock.sendto(bytes(line, encoding='UTF-8'), (UDP_IP, UDP_PORT))
+        ackNum = 0
+        #while there are less acknowledged packets than the max window
+        while ackNum < windowSize:
+            sendWindow(window, IP, PORT, ackNum)
+            ackNum = receiveAcknowledgment(IP, PORT, sock)
+    
+    #Once the data has completed being sent, send the terminate message
+    sock.sendto(bytes("ffff", encoding='UTF-8'), (localIP, PORT))
 
     #else:
         #print("Usage:\t python3 client.py [IP] tux.txt")
--- project4/server.py
+++ project4/server.py
@@ -3,22 +3,74 @@
 import socket
 from scapy.all import *
 
-UDP_IP = "10.0.0.2"
-UDP_PORT = 5005
-outfile = open("output.txt", "a+")
+def getLocalIP():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(("10.0.0.1", 80))
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+    localIP = sock.getsockname()[0]
+    print(sock.getsockname()[0])
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    if len(data.decode("UTF-8")) > 0:
-        print(data.decode("UTF-8"), end = '')
-        outfile.write(data.decode("UTF-8"))
+    sock.close()
+
+    return localIP 
+
+if __name__ == "__main__":
+
+    if (len(sys.argv) < 2): #if the file arg is not passed, exit
+        print("!Invalid Input Parameters!\nproper input:\tpython3 server.py <filename>")
+        exit(1)
+
+    IP = getLocalIP()
+    PORT = 5005
+    outfile = open(sys.argv[1], "a+")
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+    sock.bind((IP, PORT))
+    sock.settimeout(5)
+
+    nextSeqNumber = 0
+    print("Awaiting Data Transfer")
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)        # buffer size is 1024 bytes
+            data = data.decode("UTF-8")             #decode the byte sequence
+            message = data[1:]                      #slice off the seq number to get message
+
+            #if a packet is recieved containing "ffff", close the socket and exit
+            if data == "ffff" or data[1:] == "ffff":
+                print("Transmission Complete")
+                sock.close()
+                exit(0)
+
+            #pull the sequence number from the packet and compare
+            #to the next expected packet
+            seqNum = int(data[0])
+            if nextSeqNumber == seqNum: #if it is equal to the next expected packet, write
+                outfile.write(message)
+                nextSeqNumber +=1
+            else:                       #otherwise send a NACK, for a dropped packet
+                print("Packet dropped in transmission: ")
+                print(nextSeqNumber)
+                nack = "N:"+str(nextSeqNumber)
+                sock.sendto(bytes(nack, encoding='UTF-8'), (addr[0], PORT))
+
+            if nextSeqNumber == 10: #if the nextSeqNumber is 10, reset, a full window will has been recieved
+                print("ACK Sent")
+                nextSeqNumber = 0
+                sock.sendto(bytes("ACK", encoding='UTF-8'), (addr[0], PORT))
     
-    # Packet.__class__(bytes(data))
-    # data.Show()
-    #pckt=Raw(data)
-    #pckt.show()
+        except(socket.timeout):
+            print("Socket Timed Out, Exiting Program")
+            sock.close()
+            exit(1)
+
+            #if len(data.decode("UTF-8")) > 0:
+             #   print(data.decode("UTF-8"), end = '')
+              #  outfile.write(data.decode("UTF-8"))
+        
+        # Packet.__class__(bytes(data))
+        # data.Show()
+        #pckt=Raw(data)
+        #pckt.show()
 
